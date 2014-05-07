37b123688652b5f24424ee9d9e2f171cd02734fd
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2014-05-07 14:11:14 +0800

    fix module name in file source; move config from manifest into module

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,9 +2,13 @@
 class hpacucli () {
 
   file {'/usr/bin/hpatool.sh':
-    source => 'puppet:///modules/hpa/hpatool.sh',
+    source => 'puppet:///modules/hpacucli/hpatool.sh',
     mode   => '0700'
   }
 
+  file {"/etc/apt/sources.list.d/hpa.list":
+    content => 'deb http://bpamirror:81/hpa ./',
+    notify => Exec['apt-get-update']
+  } ->
   exec {'/usr/bin/apt-get -q -y --allow-unauthenticated -o DPkg::Options::=--force-confold install hpacucli':}
 }
