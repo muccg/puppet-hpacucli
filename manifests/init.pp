feb65c62032504a6b4f6709eb0fe8347adad1cfa
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2014-05-07 14:56:24 +0800

    use repo definition for apt source

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,9 +6,6 @@ class hpacucli () {
     mode   => '0700'
   }
 
-  file {"/etc/apt/sources.list.d/hpa.list":
-    content => 'deb http://bpamirror:81/hpa ./',
-    notify => Exec['apt-get-update']
-  } ->
+  class {"repo::hpacucli":} ->
   exec {'/usr/bin/apt-get -q -y --allow-unauthenticated -o DPkg::Options::=--force-confold install hpacucli':}
 }
